572bbb4bf6aa8ede41c6d7af4010b0251b05b789
rishabhallawadi <rishabhallawadi3@gmail.com>
2023-04-20 17:56:53 +0530

    test demo

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -72,6 +72,15 @@ $( "#signup-from" ).submit(function( event ) {
 
   auth
     .createUserWithEmailAndPassword(email, password)
+    .then(cred => {
+      console.log(cred.user);
+      return db.collection("users")
+          .add({
+            name,
+            uuid: cred.user.uid
+          })
+
+      })
     .then((data) => {
       window.location.href = "index.html" 
     })
